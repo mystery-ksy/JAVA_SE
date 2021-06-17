50e50042e769c190d08bedb2ff3ba3fee673815a
mystery <mystery@ksy.com>
2021-06-17 21:20:09 +0800

    second commit

--- src/com/mystery/Demo.java
+++ src/com/mystery/Demo.java
@@ -3,5 +3,6 @@ package com.mystery;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("one---------");
+        System.out.println("two---------");
     }
 }
